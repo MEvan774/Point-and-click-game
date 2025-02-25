4754bff6241717f374e531b40fe89b226e0d5213
Arsalaan Khan <arsalaan.khan@hva.nl>
2025-02-25 10:36:30 +0100

    FEAT: Heb alvast code voor de outside room en code om van de outside room naar de omliggende rooms te gaan.
    ALLEEN IS ER NOG GEEN IMAGE!
    
    ps: heb perongeluk in de main branch gewerkt.

---
+++ src/api/src/game-implementation/rooms/OutsideRoom.ts
@@ -0,0 +1,62 @@
+import { ActionResult } from "../../game-base/actionResults/ActionResult";
+import { TextActionResult } from "../../game-base/actionResults/TextActionResult";
+import { Action } from "../../game-base/actions/Action";
+import { ExamineAction } from "../../game-base/actions/ExamineAction";
+import { GameObject } from "../../game-base/gameObjects/GameObject";
+import { Room } from "../../game-base/gameObjects/Room";
+import { GoToAction } from "../actions/GoToAction";
+import { DoorOutsideFrontdoor } from "../items/DoorOutsideFrontdoor";
+
+/**
+ * Implemention of the bedroom room
+ *
+ * @remarks Used as the first room for new player sessions.
+ */
+export class OutsideRoom extends Room {
+    public static readonly Alias: string = "outside";
+
+    /**
+     * Create a new instance of this room
+     */
+    public constructor() {
+        super(OutsideRoom.Alias);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public name(): string {
+        return "Outside the castle";
+    }
+
+    /**
+     * @inheritdoc
+     */
+    //    public images(): string[] {
+    //        return ["outsideRoom"];
+    //    }
+
+    public actions(): Action[] {
+        return [
+            new ExamineAction(),
+            new GoToAction(),
+        ];
+    }
+
+    public objects(): GameObject[] {
+        return [
+            new DoorOutsideFrontdoor(),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public examine(): ActionResult | undefined {
+        return new TextActionResult([
+            "This is outside the castle.",
+            "There is a shed that's half open.",
+            "There is a fence that's locked.",
+        ]);
+    }
+}
