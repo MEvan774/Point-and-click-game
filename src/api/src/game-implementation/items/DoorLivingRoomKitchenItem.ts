457e78d82663f90901be44c7dc7b0a0d6d7ad9a5
Milan Breuren <milan.breuren@hva.nl>
2025-03-08 14:50:18 +0100

    Style: Added comments to everything livingRoom related.

--- src/api/src/game-implementation/items/DoorLivingRoomKitchenItem.ts
+++ src/api/src/game-implementation/items/DoorLivingRoomKitchenItem.ts
@@ -23,16 +23,27 @@ export class DoorLivingRoomKitchenItem extends Item implements Examine, GoTo {
     public constructor() {
         super(DoorLivingRoomKitchenItem.Alias, DoorLivingRoomKitchenItem.validActions);
     }
+    // Name of the item, shows up on the buttons for example
 
     public name(): string {
         return "Kitchen";
     }
 
+    /**
+     * Tells about where the door leads to
+     *
+     * @returns TextActionResult with information about the item
+     */
     public examine(): ActionResult | undefined {
         return new TextActionResult(["There is blood on the door...",
             "I'm not sure if i want to see whats behind there."]);
     }
 
+    /**
+     * Go to the KitchenRoom and examines this room
+     *
+     * @returns room.examine() for the KitchenRoom
+     */
     public goto(): ActionResult | undefined {
         const livingRoom: Room = new KitchenRoom();
 
