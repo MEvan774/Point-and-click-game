d4cb5638eb987b6c97856abb16c8c8c760ba010d
Lisa <lisa.hakhoff@hva.nl>
2025-03-10 14:14:39 +0100

    Feat: Added hitbox to exit office

--- src/api/src/game-implementation/items/Doors/DoorOfficeHallwayItem.ts
+++ src/api/src/game-implementation/items/Doors/DoorOfficeHallwayItem.ts
@@ -7,10 +7,14 @@ import { gameService } from "../../../global";
 import { Room } from "../../../game-base/gameObjects/Room";
 import { HallwayRoom } from "../../rooms/HallwayRoom";
 import { PickUp } from "../../actions/PickUpAction";
+import { ActionTypes } from "../../../game-base/enums/ActionAlias";
 
 export class DoorOfficeHallwayItem extends Item implements Examine, GoTo, PickUp {
     public static readonly Alias: string = "office-hallway-door";
-
+    public _position: Vector2 = { x: -510, y: 455 };
+    public _size: Vector2 = { x: 855, y: 50 };
+    public _isDebugHitboxVisible: boolean = true;
+    public _action: ActionTypes = ActionTypes.GoTo;
     public static readonly validActions: string[] = ["examine", "go to", "pick up"];
 
     public constructor() {
@@ -22,20 +26,17 @@ export class DoorOfficeHallwayItem extends Item implements Examine, GoTo, PickUp
     }
 
     public examine(): ActionResult | undefined {
-        gameService.getPlayerSession().walkedToDesk = false;
         return new TextActionResult(["This door leads back to the hallway."]);
     }
 
     public goto(): ActionResult | undefined {
         const room: Room = new HallwayRoom();
 
-        gameService.getPlayerSession().walkedToDesk = false;
         gameService.getPlayerSession().currentRoom = room.alias;
         return room.examine();
     }
 
     public pickup(): ActionResult | undefined {
-        gameService.getPlayerSession().walkedToDesk = true;
 
         return new TextActionResult([
             "This door is very heavy, and therefor seems to be able to pack a punch! You have picked up the door.",
